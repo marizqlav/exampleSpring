c1e918baaceaa3e6e4b1f31437be8d2e1b466dd9
marizqlav <marizqlav@alum.us.es>
2025-02-04 17:28:59 +0100

    Add Spring Security dependency and configure security settings

---
+++ src/main/java/com/app/prueba/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.app.prueba.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable()).authorizeHttpRequests((requests) -> requests
+                .requestMatchers(HttpMethod.GET, "/", "api/users", "api/users/").permitAll()
+                .requestMatchers(HttpMethod.POST, "/", "api/users").permitAll()
+                .requestMatchers(HttpMethod.PUT, "/", "api/users/").permitAll()
+                .requestMatchers(HttpMethod.DELETE, "/", "api/users/*").permitAll());
+
+        return http.build();
+    }
+
+}
